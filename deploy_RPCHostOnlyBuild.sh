a9d86d539419b0bc682eb02ef91df84bef1b0a30
Rajasekhar <praja4267@gmail.com>
2020-08-25 13:30:53 +0530

    show list of items in temp folder

--- deploy_RPCHostOnlyBuild.sh
+++ deploy_RPCHostOnlyBuild.sh
@@ -53,7 +53,7 @@ echo "mounting the remotePCHost"
 hdiutil attach -nobrowse -noverify /tmp/RemotePCHost.dmg
 cp -R /Volumes/RemotePCHost /tmp
 hdiutil detach /Volumes/RemotePCHost
-echo "started installer at `date` located in `/tmp/RemotePCHost/*`"
+echo "started installer at `date` located in `ls /tmp/RemotePCHost/`"
 # installer -target / -pkg /tmp/RemotePCHost/RemotePCHost.pkg
 echo "Installation completed at `date`"
 rm -rf /tmp/RemotePCHost
